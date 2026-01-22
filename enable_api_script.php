5ed15c2ee56701d27dd6d82a1fbfaabc01111ea6
digitalcigit <git@digital.ci>
2026-01-22 23:08:52 +0000

    Remove temporary utility scripts

--- enable_api_script.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    Botble\Setting\Facades\Setting::set('api_enabled', 1);
-    Botble\Setting\Facades\Setting::save();
-    echo "API enabled successfully\n";
-} catch (\Exception $e) {
-    echo "Error: " . $e->getMessage() . "\n";
-}
--- enable_api_script_tinker.php
+++
@@ -1,7 +0,0 @@
-try {
-    Botble\Setting\Facades\Setting::set('api_enabled', 1);
-    Botble\Setting\Facades\Setting::save();
-    echo "API enabled successfully\n";
-} catch (\Exception $e) {
-    echo "Error: " . $e->getMessage() . "\n";
-}
