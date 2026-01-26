1778614fa51be1832ce52b5cc71ecc147a430b70
digitalcigit <git@digital.ci>
2026-01-26 07:07:49 +0000

    chore: remove temporary enable_api script

--- enable_api.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require 'bootstrap/app.php';
-$app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
-Botble\Setting\Facades\Setting::set('api_enabled', 1)->save();
-echo 'API enabled successfully';
